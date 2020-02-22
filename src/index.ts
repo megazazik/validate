688a9dafa3a2ac7295d60703a4a24f0aac52f470
Lebedev Mikhail <lebedev.mv@tutu.ru>
2020-02-22 18:59:10 +0300

    add meta for common cases

--- src/index.ts
+++ src/index.ts
@@ -1,313 +1,354 @@
-import { Result, getErrors } from "./result";
+import { Result, getErrors } from './result';
 
 export { getErrors };
 
-export type Rule<D, R = boolean, FullData = any> = (
-  obj: D,
-  fullData?: FullData
-) => R;
+export type Rule<D, R, Meta> = Meta extends undefined
+	? (obj: D, meta?: Meta) => R
+	: (obj: D, meta: Meta) => R;
 
-export type RuleError<K, R extends Rule<any, any>> = R extends (
-  data: any
+export type RuleError<K, R extends Rule<any, any, any>> = R extends (
+	...args: any[]
 ) => boolean
-  ? { type: K }
-  : { type: K; error: ReturnType<R> };
+	? { type: K }
+	: { type: K; error: ReturnType<R> };
 
-export type ObjectRules<T, FullData> = {
-  [R: string]: Rule<T, any, FullData>;
-};
+export type ObjectRules<T, Meta> = Record<string, Rule<T, any, Meta>>;
 
 export type RulesErrors<T, Rules extends ObjectRules<T, any>> = {
-  [R in keyof Rules]: RuleError<R, Rules[R]>;
+	[R in keyof Rules]: RuleError<R, Rules[R]>;
 };
 
-export type ValidationScheme<Data, Result, FullData = any> = {
-  validate(data: Data, fullData?: FullData): null | Result;
+export type ValidationScheme<Data, Result, Meta = undefined> = {
+	validate: (
+		...args: Meta extends undefined ? [Data, any?] : [Data, Meta]
+	) => null | Result;
 };
 
-type ChildrenRules<T> = {
-  [F in keyof T]?: ValidationScheme<T[F], any, T> | ObjectRules<T[F], T>;
+type ChildMeta<T, Meta> = {
+	data: T;
+	meta: Meta;
+	fieldName: keyof T;
 };
 
-type ObjectRulesNames<D, C extends SchemeRules<D>> = {
-  [F in keyof C]: C[F] extends Rule<D, any> ? F : never;
+type ChildrenRules<T, Meta> = {
+	[F in keyof T]?:
+		| ValidationScheme<T[F], any, ChildMeta<T, Meta>>
+		| ObjectRules<T[F], ChildMeta<T, Meta>>;
+};
+
+type ObjectRulesNames<D, C extends SchemeRules<D, any>> = {
+	[F in keyof C]: C[F] extends Rule<D, any, any> ? F : never;
 }[keyof C];
 
-type ChildRulesNames<D, C extends SchemeRules<D>> = {
-  [F in keyof C]: F extends keyof D ? F : never;
+type ChildRulesNames<D, C extends SchemeRules<D, any>> = {
+	[F in keyof C]: F extends keyof D ? F : never;
 }[keyof C];
 
 type CheckIfEmptyKeys<Keys, Success, Wrong> = Wrong extends (Keys extends never
-? Success
-: Wrong)
-  ? Wrong
-  : Success;
+	? Success
+	: Wrong)
+	? Wrong
+	: Success;
 
 type ErrorsOf<T> = { errors: Array<T[keyof T]> };
 
 export type ChildResult<
-  Children extends { [F: string]: undefined | ValidationScheme<any, any> }
+	Children extends {
+		[F: string]: undefined | ValidationScheme<any, any, any>;
+	}
 > = {
-  [F in keyof Children]?: Children[F] extends ValidationScheme<any, any>
-    ? ReturnType<Children[F]["validate"]>
-    : never;
+	[F in keyof Children]?: Children[F] extends ValidationScheme<any, any, any>
+		? ReturnType<Children[F]['validate']>
+		: never;
 };
 
 export type ValidationResult<
-  Data,
-  Rules extends { [R: string]: Rule<Data, any> },
-  Children extends { [F in keyof Data]?: ValidationScheme<Data[F], any, Data> }
+	Data,
+	Rules extends { [R: string]: Rule<Data, any, any> },
+	Children extends { [F in keyof Data]?: ValidationScheme<Data[F], any, any> }
 > = null | Readonly<
-  ChildResult<Children> &
-    ErrorsOf<RulesErrors<any, Rules>> &
-    { [R in keyof Rules]?: ReturnType<Rules[R]> }
+	ChildResult<Children> &
+		ErrorsOf<RulesErrors<any, Rules>> &
+		{ [R in keyof Rules]?: ReturnType<Rules[R]> }
 >;
 
-export type SchemeRules<D> = Record<
-  string,
-  Rule<D, any> | ValidationScheme<any, any> | { [R: string]: Rule<any, any, D> }
+export type SchemeRules<D, Meta> = Record<
+	string,
+	| Rule<D, any, Meta>
+	| ValidationScheme<any, any, any>
+	| {
+			[R: string]: Rule<any, any, any>;
+	  }
 >;
 
 export interface Scheme<
-  Data,
-  Rules extends ObjectRules<Data, unknown>,
-  Children extends {
-    [F in keyof Data]?: ValidationScheme<Data[F], any, Data>;
-  } = {}
-> {
-  validate(data: Data): ValidationResult<Data, Rules, Children>;
-  rules<C extends SchemeRules<Data>>(
-    children: C & ChildrenRules<Data>
-  ): CheckIfEmptyKeys<
-    Exclude<keyof C, ChildRulesNames<Data, C> | ObjectRulesNames<Data, C>>,
-    Scheme<
-      Data,
-      Rules & Pick<C, ObjectRulesNames<Data, C>>,
-      Children & ChildrenSchemes<Data, Pick<C, ChildRulesNames<Data, C>>>
-    >,
-    | "Wrong type of rules. Check the following fields:"
-    | Exclude<keyof C, ChildRulesNames<Data, C> | ObjectRulesNames<Data, C>>
-  >;
+	Data,
+	Rules extends ObjectRules<Data, Meta>,
+	Children extends {
+		[F in keyof Data]?: ValidationScheme<Data[F], any, Data>;
+	} = {},
+	Meta = undefined
+>
+	extends ValidationScheme<
+		Data,
+		ValidationResult<Data, Rules, Children>,
+		Meta
+	> {
+	rules<C extends SchemeRules<Data, Meta>>(
+		children: C & ChildrenRules<Data, Meta>
+	): CheckIfEmptyKeys<
+		Exclude<keyof C, ChildRulesNames<Data, C> | ObjectRulesNames<Data, C>>,
+		Scheme<
+			Data,
+			Rules & Pick<C, ObjectRulesNames<Data, C>>,
+			Children & ChildrenSchemes<Data, Pick<C, ChildRulesNames<Data, C>>>,
+			Meta
+		>,
+		| 'Wrong type of rules. Check the following fields:'
+		| Exclude<keyof C, ChildRulesNames<Data, C> | ObjectRulesNames<Data, C>>
+	>;
 }
 
 export interface PrimitiveScheme<
-  Data,
-  Rules extends ObjectRules<Data, unknown>
-> {
-  validate(data: Data): ValidationResult<Data, Rules, {}>;
-  rules<Rules extends Record<string, Rule<Data, any>>>(
-    children: Rules
-  ): PrimitiveScheme<Data, Rules & Rules>;
+	Data,
+	Rules extends ObjectRules<Data, unknown>,
+	Meta = undefined
+> extends ValidationScheme<Data, ValidationResult<Data, Rules, {}>, Meta> {
+	rules<NewRules extends Record<string, Rule<Data, any, Meta>>>(
+		children: NewRules
+	): PrimitiveScheme<Data, Rules & NewRules, Meta>;
 }
 
-export type ChildrenSchemes<Data, Rules extends ChildrenRules<any>> = {
-  [Child in keyof Rules]: ChildScheme<
-    Rules[Child],
-    Child extends keyof Data ? Data[Child] : any
-  >;
+export type ChildrenSchemes<Data, Rules extends ChildrenRules<any, any>> = {
+	[Child in keyof Rules]: ChildScheme<
+		Rules[Child],
+		Child extends keyof Data ? Data[Child] : any
+	>;
 };
 
 export type ChildScheme<
-  C extends
-    | ValidationScheme<any, any>
-    | { [R: string]: Rule<any, any> }
-    | undefined,
-  Data
+	C extends
+		| ValidationScheme<any, any, any>
+		| { [R: string]: Rule<any, any, any> }
+		| undefined,
+	Data
 > = C extends ValidationScheme<any, any>
-  ? C
-  : Scheme<Data, C extends undefined ? {} : C, {}>;
+	? C
+	: Scheme<Data, C extends undefined ? {} : C, {}>;
 
-export function init<Data>(): Data extends object
-  ? Scheme<Data, {}, {}>
-  : PrimitiveScheme<Data, {}> {
-  return new Builder<Data, {}, {}>({}, {}) as any;
+export function init<Data, Meta = undefined>(): Data extends object
+	? Scheme<Data, {}, {}, Meta>
+	: PrimitiveScheme<Data, {}, Meta> {
+	return new Builder<Data, {}, {}>({}, {}) as any;
 }
 
 export type RulesOfScheme<S extends Scheme<any, any, any>> = S extends Scheme<
-  any,
-  infer R,
-  any
+	any,
+	infer R,
+	any
 >
-  ? R
-  : never;
+	? R
+	: never;
 
 export type ChildrenOfScheme<
-  S extends Scheme<any, any, any>
+	S extends Scheme<any, any, any>
 > = S extends Scheme<any, any, infer R> ? R : never;
 
 class Builder<
-  Data,
-  Rules extends { [R: string]: Rule<Data, any> },
-  Children extends {
-    [F in keyof Data]?: ValidationScheme<Data[F], any, Data>;
-  } = {}
-> implements Scheme<Data, Rules, Children> {
-  constructor(private _fullObjectRules: Rules, private _rules: Children) {}
+	Data,
+	Rules extends { [R: string]: Rule<Data, any, Meta> },
+	Children extends {
+		[F in keyof Data]?: ValidationScheme<Data[F], any, Data>;
+	} = {},
+	Meta = undefined
+> implements Scheme<Data, Rules, Children, Meta> {
+	constructor(private _fullObjectRules: Rules, private _rules: Children) {}
 
-  rules<C extends SchemeRules<Data>>(fullRules: C & ChildrenRules<Data>) {
-    const [childrenRules, rules] = separateRules(fullRules);
-    const convertedRules = Object.keys(childrenRules).reduce(
-      (prev, propertyName) => ({
-        ...prev,
-        [propertyName]:
-          typeof (childrenRules as any)[propertyName].validate === "function"
-            ? (childrenRules as any)[propertyName]
-            : init<object>().rules((childrenRules as any)[propertyName])
-      }),
-      {}
-    );
+	rules<C extends SchemeRules<Data, Meta>>(
+		fullRules: C & ChildrenRules<Data, Meta>
+	) {
+		const [childrenRules, rules] = separateRules(fullRules);
+		const convertedRules = Object.keys(childrenRules).reduce(
+			(prev, propertyName) => ({
+				...prev,
+				[propertyName]:
+					typeof (childrenRules as any)[propertyName].validate ===
+					'function'
+						? (childrenRules as any)[propertyName]
+						: init<object>().rules(
+								(childrenRules as any)[propertyName]
+						  )
+			}),
+			{}
+		);
 
-    return new Builder(
-      { ...this._fullObjectRules, ...rules },
-      { ...this._rules, ...convertedRules }
-    ) as any;
-  }
+		return new Builder(
+			{ ...this._fullObjectRules, ...rules },
+			{ ...this._rules, ...convertedRules }
+		) as any;
+	}
 
-  validate(data: Data): ValidationResult<Data, Rules, Children> {
-    let hasErrors = false;
-    const ownRulesErrors = validateRules(data, this._fullObjectRules);
-    if (ownRulesErrors) {
-      hasErrors = true;
-    }
+	validate(
+		...args: Meta extends undefined ? [Data, any?] : [Data, Meta]
+	): ValidationResult<Data, Rules, Children> {
+		let hasErrors = false;
+		const ownRulesErrors = validateRules(
+			args[0],
+			this._fullObjectRules,
+			args[1]!
+		);
+		if (ownRulesErrors) {
+			hasErrors = true;
+		}
 
-    const errors: any = new Result(ownRulesErrors || {});
+		const errors: any = new Result(ownRulesErrors || {});
 
-    Object.keys(this._rules).forEach(key => {
-      const res = (this._rules as any)[key].validate((data as any)[key]);
-      if (res) {
-        hasErrors = true;
-        errors[key] = res;
-      }
-    });
-    return hasErrors ? errors : null;
-  }
+		Object.keys(this._rules).forEach(key => {
+			const res = (this._rules as any)[key].validate(
+				(args[0] as any)[key],
+				{
+					data: args[0],
+					meta: args[1],
+					fieldName: key
+				}
+			);
+			if (res) {
+				hasErrors = true;
+				errors[key] = res;
+			}
+		});
+		return hasErrors ? errors : null;
+	}
 }
 
 function separateRules(
-  rules: SchemeRules<any>
-): [ChildrenRules<any>, Record<string, Rule<any, any>>] {
-  const childrenRules: ChildrenRules<any> = {};
-  const objectRules: Record<string, Rule<any, any>> = {};
+	rules: SchemeRules<any, any>
+): [ChildrenRules<any, any>, Record<string, Rule<any, any, any>>] {
+	const childrenRules: ChildrenRules<any, any> = {};
+	const objectRules: Record<string, Rule<any, any, any>> = {};
 
-  for (const key in rules) {
-    const rule = rules[key];
-    if (typeof rule === "function") {
-      objectRules[key] = rule;
-    } else {
-      childrenRules[key] = rule;
-    }
-  }
+	for (const key in rules) {
+		const rule = rules[key];
+		if (typeof rule === 'function') {
+			objectRules[key] = rule;
+		} else {
+			childrenRules[key] = rule as any;
+		}
+	}
 
-  return [childrenRules, objectRules];
+	return [childrenRules, objectRules];
 }
 
-export function validateRules<D>(
-  data: D,
-  rules: { [R: string]: Rule<D, any> }
+export function validateRules<D, Meta = undefined>(
+	data: D,
+	rules: { [R: string]: Rule<D, any, Meta> },
+	meta: Meta
 ) {
-  let errors: any = null;
+	let errors: any = null;
 
-  Object.keys(rules).find(key => {
-    if ((rules[key] as any).isAllOfFlag === isAllOfFlag) {
-      const allOfRules = rules[key](data);
+	Object.keys(rules).find(key => {
+		if ((rules[key] as any).isAllOfFlag === isAllOfFlag) {
+			const allOfRules = rules[key](data, meta);
 
-      const allErrors: any = {};
-      let hasError = false;
-      Object.keys(allOfRules).forEach(ruleKey => {
-        const res = allOfRules[ruleKey](data);
-        if (res === false || res == null) {
-          return;
-        }
-        hasError = true;
-        allErrors[ruleKey] = res;
-      });
+			const allErrors: any = {};
+			let hasError = false;
+			Object.keys(allOfRules).forEach(ruleKey => {
+				const res = allOfRules[ruleKey](data, meta);
+				if (res === false || res == null) {
+					return;
+				}
+				hasError = true;
+				allErrors[ruleKey] = res;
+			});
 
-      if (hasError) {
-        errors = allErrors;
-        return true;
-      }
+			if (hasError) {
+				errors = allErrors;
+				return true;
+			}
 
-      return false;
-    }
+			return false;
+		}
 
-    const result = rules[key](data);
-    if (result === false || result == null) {
-      return false;
-    }
+		const result = rules[key](data, meta);
+		if (result === false || result == null) {
+			return false;
+		}
 
-    errors = { [key]: result };
+		errors = { [key]: result };
 
-    return true;
-  });
+		return true;
+	});
 
-  return errors;
+	return errors;
 }
 
 // все из списка правил
 
 const isAllOfFlag = {};
 
-export function allOf<T, Rules extends { [R: string]: Rule<T, any> }>(
-  rules: Rules
-): Rules {
-  return Object.keys(rules).reduce((prev, key, index) => {
-    if (index === 0) {
-      const rule: any = () => rules;
-      rule.isAllOfFlag = isAllOfFlag;
-      return { ...prev, [key]: rule };
-    }
+export function allOf<
+	T,
+	Rules extends { [R: string]: Rule<T, any, Meta> },
+	Meta
+>(rules: Rules): Rules {
+	return Object.keys(rules).reduce((prev, key, index) => {
+		if (index === 0) {
+			const rule: any = () => rules;
+			rule.isAllOfFlag = isAllOfFlag;
+			return { ...prev, [key]: rule };
+		}
 
-    return { ...prev, [key]: () => false };
-  }, {}) as any;
+		return { ...prev, [key]: () => false };
+	}, {}) as any;
 }
 
 // правила для массивов
 
 export function list<S extends ValidationScheme<Data, any, Data[]>, Data>(
-  scheme: S
-): ValidationScheme<Data[], Array<ReturnType<S["validate"]>>> {
-  return {
-    validate: (data: Data[]) => {
-      let hasError = false;
-      const result = data.map(d => {
-        const res = scheme.validate(d);
-        if (res) {
-          hasError = true;
-        }
-        return res;
-      });
-      return hasError ? result : null;
-    }
-  };
+	scheme: S
+): ValidationScheme<Data[], Array<ReturnType<S['validate']>>> {
+	return {
+		validate: (data: Data[]) => {
+			let hasError = false;
+			const result = data.map(d => {
+				const res = scheme.validate(d, data);
+				if (res) {
+					hasError = true;
+				}
+				return res;
+			});
+			return hasError ? result : null;
+		}
+	};
 }
 
 // правила для объектов
 
 export function map<
-  S extends ValidationScheme<Data, any, Record<string, Data>>,
-  Data
+	S extends ValidationScheme<Data, any, Record<string, Data>>,
+	Data
 >(
-  scheme: S
+	scheme: S
 ): ValidationScheme<
-  Record<string, Data>,
-  Record<string, ReturnType<S["validate"]>>
+	Record<string, Data>,
+	Record<string, ReturnType<S['validate']>>
 > {
-  return {
-    validate: (data: { [name: string]: Data }) => {
-      let hasError = false;
-      const result = Object.keys(data).reduce((prev, name) => {
-        const res = scheme.validate(data[name]);
+	return {
+		validate: (data: { [name: string]: Data }) => {
+			let hasError = false;
+			const result = Object.keys(data).reduce((prev, name) => {
+				const res = scheme.validate(data[name], data);
 
-        if (res) {
-          hasError = true;
-          return {
-            ...prev,
-            [name]: res
-          };
-        }
-        return prev;
-      }, {});
-      return hasError ? result : null;
-    }
-  };
+				if (res) {
+					hasError = true;
+					return {
+						...prev,
+						[name]: res
+					};
+				}
+				return prev;
+			}, {});
+			return hasError ? result : null;
+		}
+	};
 }
--- src/tests-d/index.test-d.ts
+++ src/tests-d/index.test-d.ts
@@ -1,192 +1,244 @@
-import { expectType, expectError } from "tsd";
-import { init } from "..";
+import { expectType, expectError } from 'tsd';
+import { init } from '..';
 
 /** Simple value */
 expectType<Readonly<{
-  errors: Array<
-    | {
-        type: "required";
-      }
-    | {
-        type: "maxLength";
-      }
-    | {
-        type: "noType";
-      }
-  >;
-  required?: boolean;
-  maxLength?: boolean;
-  noType?: boolean;
+	errors: Array<
+		| {
+				type: 'required';
+		  }
+		| {
+				type: 'maxLength';
+		  }
+		| {
+				type: 'noType';
+		  }
+	>;
+	required?: boolean;
+	maxLength?: boolean;
+	noType?: boolean;
 }> | null>(
-  init<string>()
-    .rules({
-      required: () => false,
-      maxLength: (v: string) => !!v,
-      noType: v => !!v
-    })
-    .validate("")
+	init<string>()
+		.rules({
+			required: () => false,
+			maxLength: (v: string) => !!v,
+			noType: v => !!v
+		})
+		.validate('')
+);
+
+/** Simple value with meta */
+expectType<Readonly<{
+	errors: Array<
+		| {
+				type: 'required';
+		  }
+		| {
+				type: 'maxLength';
+		  }
+		| {
+				type: 'noType';
+		  }
+	>;
+	required?: boolean;
+	maxLength?: boolean;
+	noType?: boolean;
+}> | null>(
+	init<string, number>()
+		.rules({
+			required: () => false,
+			maxLength: (v: string, n: number) => !!v,
+			noType: (v, n) => !!v
+		})
+		.validate('', 12)
 );
 
 /** Simple value - rules return data */
 expectType<Readonly<{
-  errors: Array<
-    | {
-        type: "required";
-        error: number;
-      }
-    | {
-        type: "maxLength";
-        error: string;
-      }
-    | {
-        type: "noType";
-        error: { data: number };
-      }
-  >;
-  required?: number;
-  maxLength?: string;
-  noType?: { data: number };
+	errors: Array<
+		| {
+				type: 'required';
+				error: number;
+		  }
+		| {
+				type: 'maxLength';
+				error: string;
+		  }
+		| {
+				type: 'noType';
+				error: { data: number };
+		  }
+	>;
+	required?: number;
+	maxLength?: string;
+	noType?: { data: number };
 }> | null>(
-  init<string>()
-    .rules({
-      required: () => 11,
-      maxLength: (v: string) => "sdf",
-      noType: v => ({ data: 1 })
-    })
-    .validate("")
+	init<string>()
+		.rules({
+			required: () => 11,
+			maxLength: (v: string) => 'sdf',
+			noType: v => ({ data: 1 })
+		})
+		.validate('')
 );
 
 /** object */
 expectType<Readonly<{
-  errors: never[];
-  value?: Readonly<{
-    errors: Array<
-      | {
-          type: "required";
-          error: string;
-        }
-      | {
-          type: "maxLength";
-        }
-      | {
-          type: "noType";
-        }
-      | {
-          type: "maxLengthFull";
-        }
-      | {
-          type: "maxLengthFull2";
-        }
-    >;
-    required?: string;
-    maxLength?: boolean;
-    noType?: boolean;
-    maxLengthFull?: boolean;
-    maxLengthFull2?: boolean;
-  }> | null;
+	errors: never[];
+	value?: Readonly<{
+		errors: Array<
+			| {
+					type: 'required';
+					error: string;
+			  }
+			| {
+					type: 'maxLength';
+			  }
+			| {
+					type: 'noType';
+			  }
+			| {
+					type: 'maxLengthFull';
+			  }
+			| {
+					type: 'maxLengthFull2';
+			  }
+		>;
+		required?: string;
+		maxLength?: boolean;
+		noType?: boolean;
+		maxLengthFull?: boolean;
+		maxLengthFull2?: boolean;
+	}> | null;
 }> | null>(
-  init<{ value: string }>()
-    .rules({
-      value: {
-        required: () => "",
-        maxLength: (v: string) => !!v,
-        noType: v => !!v,
-        maxLengthFull: (v: string, obj?: { value: string }) => !!v,
-        maxLengthFull2: (v: string, obj?) => !!v
-      }
-    })
-    .validate({ value: "" })
+	init<{ value: string }, { value: number }>()
+		.rules({
+			value: {
+				required: () => '',
+				maxLength: (v: string) => !!v,
+				noType: v => !!v,
+				maxLengthFull: (
+					v: string,
+					obj?: {
+						data: { value: string };
+						meta: { value: number };
+						fieldName: 'value';
+					}
+				) => !!v,
+				maxLengthFull2: (v, { data, meta, fieldName }) => !!v
+			}
+		})
+		.validate({ value: '' }, { value: 21 })
 );
 
 /** nested schemes */
 expectType<Readonly<{
-  errors: never[];
-  value?: Readonly<{
-    errors: Array<
-      | {
-          type: "required";
-          error: string;
-        }
-      | {
-          type: "maxLength";
-        }
-      | {
-          type: "noType";
-        }
-    >;
-    required?: string;
-    maxLength?: boolean;
-    noType?: boolean;
-  }> | null;
+	errors: never[];
+	value?: Readonly<{
+		errors: Array<
+			| {
+					type: 'required';
+					error: string;
+			  }
+			| {
+					type: 'maxLength';
+			  }
+			| {
+					type: 'noType';
+			  }
+		>;
+		required?: string;
+		maxLength?: boolean;
+		noType?: boolean;
+	}> | null;
 }> | null>(
-  init<{ value: string }>()
-    .rules({
-      value: init<string>().rules({
-        required: () => "",
-        maxLength: (v: string) => !!v,
-        noType: v => !!v
-      })
-    })
-    .validate({ value: "" })
+	init<{ value: string }>()
+		.rules({
+			value: init<string>().rules({
+				required: v => String(v),
+				maxLength: (v: string) => !!v,
+				noType: v => !!v
+			})
+		})
+		.validate({ value: '' })
 );
 
 /** nested schemes - own realization scheme */
 expectType<Readonly<{
-  errors: never[];
-  value?: { valueErrors: boolean };
+	errors: never[];
+	value?: { valueErrors: boolean };
 }> | null>(
-  init<{ value: string }>()
-    .rules({
-      value: { validate: () => ({ valueErrors: true }) }
-    })
-    .validate({ value: "" })
+	init<{ value: string }>()
+		.rules({
+			value: { validate: () => ({ valueErrors: true }) }
+		})
+		.validate({ value: '' })
 );
 
 /** nested schemes - own realization scheme with full object */
 expectType<Readonly<{
-  errors: never[];
-  value?: { valueErrors: boolean };
+	errors: never[];
+	value?: { valueErrors: boolean };
+	value2?: { valueErrors: boolean };
 }> | null>(
-  init<{ value: string }>()
-    .rules({
-      value: { validate: (v, obj) => ({ valueErrors: true }) }
-    })
-    .validate({ value: "" })
+	init<{ value: string; value2: string }, number>()
+		.rules({
+			value: {
+				validate: (
+					v,
+					obj: {
+						meta: number;
+						data: { value: string; value2: string };
+						fieldName: 'value';
+					}
+				) => ({ valueErrors: true })
+			},
+			value2: { validate: (v, obj) => ({ valueErrors: true }) }
+		})
+		.validate({ value: '', value2: '' }, 1)
 );
 
 /** errors */
 expectError(
-  init<string>()
-    .rules({ required: (v: number) => !!v })
-    .validate("")
+	init<string>()
+		.rules({ required: (v: number) => !!v })
+		.validate('')
 );
 
 expectError(
-  init<string>()
-    .rules({ required: "sdfsdf" })
-    .validate("")
+	init<string>()
+		.rules({ required: 'sdfsdf' })
+		.validate('')
 );
 
 expectError(
-  init<{ v: string }>()
-    .rules({ v: () => true })
-    .validate({ v: "" })
+	init<{ v: string }>()
+		.rules({ v: () => true })
+		.validate({ v: '' })
 );
 
 expectError(
-  init<{ v: string }>()
-    .rules({
-      v: {
-        req: (v: number) => false
-      }
-    })
-    .validate({ v: "" })
+	init<{ v: string }>()
+		.rules({
+			v: {
+				req: (v: number) => false
+			}
+		})
+		.validate({ v: '' })
 );
 
-expectType<"Wrong type of rules. Check the following fields:" | "d">(
-  init<{ v: string }>().rules({
-    d: {
-      req: (v: number) => false
-    }
-  })
+expectError(
+	init<{ value: string }, number>().validate({
+		value: ''
+	})
 );
+
+expectType<'Wrong type of rules. Check the following fields:' | 'd'>(
+	init<{ v: string }>().rules({
+		d: {
+			req: (v: number) => false
+		}
+	})
+);
+
+/** @todo типы для list и map */
